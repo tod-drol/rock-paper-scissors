487aecb5f8f7199f9dac6d4607fc7e6d9f5aa5d6
tod-drol <tod.dark13@gmail.com>
2024-07-10 23:42:47 -0700

    Add the not so elegant solution to the rock paper scissors round based game

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// This variables stores the winner's score, once the score reaches five an absolute winner will rise to glory!
 let humanScore = 0;
 let computerScore = 0;
 
+// function stores the computer choice which is randomly generated
 function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * 3);
 
@@ -13,18 +15,60 @@ function getComputerChoice() {
   }
 }
 
+// function stores the value the user inputs through the prompt
 function getHumanChoice() {
-  let humanChoice = prompt("Rock Paper or Scissors?").toLowerCase();
+  let humanChoice = prompt("Rock Paper or Scissors?");
   return humanChoice;
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame() {
+  // function plays single rounds using conditional statements and the stored values in the selection variables to select a winner and increases the winner's score
+  function playRound(humanSelection, computerSelection) {
+    if (humanSelection === "rock" && computerSelection === "scissors") {
+      humanScore++;
+      console.log("You Won this round!");
+    } else if (humanSelection === "paper" && computerSelection === "rock") {
+      humanScore++;
+      console.log("You Won this round!");
+    } else if (humanSelection === "scissors" && computerSelection === "paper") {
+      humanScore++;
+      console.log("You Won this round!");
+    } else if (
+      (humanSelection === "rock" && computerSelection === "rock") ||
+      (humanSelection === "paper" && computerSelection === "paper") ||
+      (humanSelection === "scissors" && computerSelection === "scissors")
+    ) {
+      console.log("You tied this round!");
+    } else {
+      computerScore++;
+      console.log("Computer won this round!");
+    }
+  }
 
-function playRound(humanSelection, computerSelection) {
-    return `The human chose: ${humanSelection} and the computer chose: ${computerSelection}` ;
+  // stores the value both functions return and use it to define the round winner
+  // const humanSelection = getHumanChoice().toLowerCase();
+  //   playRound(getHumanChoice().toLowerCase(), getComputerChoice().toLowerCase());
+  playRound("rock", getComputerChoice().toLowerCase());
+
+  console.log(
+    `${"your score: " + humanScore + " Computer's score: " + computerScore}`
+  );
+
+  if (humanScore == 5) {
+    console.log("You win the game!");
+    return;
+  }
+
+  if (computerScore == 5) {
+    console.log("Computer gets to destroy the world because you lost!");
+    return;
+  }
+
+  playGame();
 }
 
 // console.log(`The computer chose: ${getComputerChoice()}`);
 // console.log(getHumanChoice());
-console.log(playRound(humanSelection, computerSelection));
+// console.log(playRound(humanSelection, computerSelection));
+
+playGame();
